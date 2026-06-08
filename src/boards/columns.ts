e9cba5df78d97b4e32bac0bab939e23ccf52f37b
Lim Zi Yang <yangz030715@gmail.com>
2026-06-08 13:45:38 +0800

    feat: added detailed permission

--- src/boards/columns.ts
+++ src/boards/columns.ts
@@ -8,5 +8,16 @@ export const COLUMN_CATEGORY = {
   CANCELLED: 6,
 } as const;
 
+export const EXPENSES_CLAIM_COLUMN_CATEGORY = {
+  DRAFT: 0,
+  PENDING: 1,
+  APPROVED: 2,
+  REJECTED: 3,
+  REOPENED: 4,
+} as const;
+
 export type ColumnCategory =
   (typeof COLUMN_CATEGORY)[keyof typeof COLUMN_CATEGORY];
+
+export type ExpensesClaimColumnCategory =
+  (typeof EXPENSES_CLAIM_COLUMN_CATEGORY)[keyof typeof EXPENSES_CLAIM_COLUMN_CATEGORY];
--- src/permissions.ts
+++ src/permissions.ts
@@ -4,9 +4,17 @@ export const PERMISSION = {
   PROJECT_CREATE: "project.create",
   PROJECT_UPDATE: "project.update",
   PROJECT_DELETE: "project.delete",
-  ISSUE_CREATE: "issue.create",
-  ISSUE_UPDATE: "issue.update",
-  ISSUE_DELETE: "issue.delete",
+  ISSUE_KANBAN_CREATE: "issue.kanban.create",
+  ISSUE_KANBAN_UPDATE: "issue.kanban.update",
+  ISSUE_KANBAN_DELETE: "issue.kanban.delete",
+  ISSUE_EXPENSES_CLAIM_CREATE: "issue.expenses_claim.create",
+  ISSUE_EXPENSES_CLAIM_EDIT: "issue.expenses_claim.edit",
+  ISSUE_EXPENSES_CLAIM_UPDATE: "issue.expenses_claim.update",
+  ISSUE_EXPENSES_CLAIM_DELETE: "issue.expenses_claim.delete",
+  ISSUE_VENUE_BOOKING_CREATE: "issue.venue_booking.create",
+  ISSUE_VENUE_BOOKING_EDIT: "issue.venue_booking.edit",
+  ISSUE_VENUE_BOOKING_UPDATE: "issue.venue_booking.update",
+  ISSUE_VENUE_BOOKING_DELETE: "issue.venue_booking.delete",
 } as const;
 
 export type Permission = (typeof PERMISSION)[keyof typeof PERMISSION];
